7b0b38d47ac8ce800e0139cc2bf2260f4d15b2c5
Eric Espie <eric.espie@combodo.com>
2022-07-06 10:11:53 +0200

    Cron parallelization  * refactor counts

--- webservices/cron.php
+++ webservices/cron.php
@@ -306,7 +306,7 @@ function CronExec($oP, $bVerbose, $bDebug=false)
 			}
 
 			// Tasks to run later
-			if ($bVerbose && $aTasks == [])
+			if ($bVerbose && count($aTasks) == 0)
 			{
 				$oP->p('--');
 				$oSearch = new DBObjectSearch('BackgroundTask');
@@ -322,7 +322,7 @@ function CronExec($oP, $bVerbose, $bDebug=false)
 				}
 			}
 		}
-		if ($aTasks == []) {
+		if (count($aTasks) == 0) {
 			if ($bVerbose) {
 				$oP->p("Sleeping...\n");
 			}
