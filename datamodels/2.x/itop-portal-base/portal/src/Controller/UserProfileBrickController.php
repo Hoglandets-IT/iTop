8980f627e90a841c21ebdd57f06570c92d5aa3cc
jf-cbd <jimmy.fayolle@combodo.com>
2025-01-21 12:09:06 +0100

    Fix format

--- datamodels/2.x/itop-portal-base/portal/src/Controller/UserProfileBrickController.php
+++ datamodels/2.x/itop-portal-base/portal/src/Controller/UserProfileBrickController.php
@@ -132,8 +132,7 @@ class UserProfileBrickController extends BrickController
 			$sCurContactId = $oCurContact->GetKey();
 
 			// Preparing forms
-			$aData['forms']['contact'] = $ObjectFormHandler->HandleForm($oRequest, $sFormMode, $sCurContactClass, $sCurContactId,
-				);
+			$aData['forms']['contact'] = $ObjectFormHandler->HandleForm($oRequest, $sFormMode, $sCurContactClass, $sCurContactId);
 			$aData['forms']['preferences'] = $this->HandlePreferencesForm($oRequest, $sFormMode);
 			// - If user can change password, we display the form
 			$aData['forms']['password'] = (UserRights::CanChangePassword()) ? $this->HandlePasswordForm($oRequest, $sFormMode) : null;
