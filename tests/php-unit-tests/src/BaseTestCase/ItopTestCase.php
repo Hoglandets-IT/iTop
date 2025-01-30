5851b2e1ff876401785deb65e21c6982442b94b6
XGUI <xavier.guiboud-ribaud@combodo.com>
2025-01-30 15:06:00 +0100

    N°8129 - Fix missing namespace backslash

--- tests/php-unit-tests/src/BaseTestCase/ItopTestCase.php
+++ tests/php-unit-tests/src/BaseTestCase/ItopTestCase.php
@@ -556,7 +556,7 @@ abstract class ItopTestCase extends TestCase
 	static protected function AssertDateEqualsNow($sActualDate, $sMessage = ''): void
 	{
 		$oActualDate = \DateTime::createFromFormat(\AttributeDate::GetInternalFormat(), $sActualDate);
-		$oNow = new DateTime();
+		$oNow = new \DateTime();
 		$iTimeInterval = $oNow->diff($oActualDate)->s;
 		self::assertLessThan(2, $iTimeInterval, $sMessage);
 	}
@@ -566,7 +566,7 @@ abstract class ItopTestCase extends TestCase
 	static protected function AssertDateTimeEqualsNow($sActualDate, $sMessage = ''): void
 	{
 		$oActualDateTime = \DateTime::createFromFormat(\AttributeDateTime::GetInternalFormat(), $sActualDate);
-		$oNow = new DateTime();
+		$oNow = new \DateTime();
 		$iTimeInterval = $oNow->diff($oActualDateTime)->s;
 		self::assertLessThan(2, $iTimeInterval, $sMessage);
 	}
