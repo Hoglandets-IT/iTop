78511097947011143dba225c6b043ee522387bb5
Anne-Cath <anne-catherine.cognet@combodo.com>
2025-09-12 10:02:12 +0200

    N°3749 - Remove old JS tooltip libs (qTip, jQuery tooltip, Bootstrap tooltip) calls - remove qtip

--- sources/Application/WebPage/iTopWebPage.php
+++ sources/Application/WebPage/iTopWebPage.php
@@ -72,8 +72,6 @@ class iTopWebPage extends NiceWebPage implements iTabbedPage
 	protected const COMPATIBILITY_DEPRECATED_LINKED_SCRIPTS_REL_PATH = [
 		'js/date.js',
 		'js/jquery.layout.min.js',
-		/** @deprecated 3.0.0 N°3748 qTip will be removed in 3.x, use Tippy.js instead */
-		'js/jquery.qtip-1.0.min.js',
 	];
 	/** @inheritDoc */
 	protected const COMPATIBILITY_MOVED_LINKED_STYLESHEETS_REL_PATH = [
