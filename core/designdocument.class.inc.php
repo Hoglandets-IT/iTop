6d5660ca67d1b3721a81c6c7f22d1081a148780b
odain <olivier.dain@combodo.com>
2025-11-15 21:28:38 +0100

    fix code style

--- core/designdocument.class.inc.php
+++ core/designdocument.class.inc.php
@@ -70,7 +70,7 @@ class DesignDocument extends DOMDocument
 		$this->preserveWhiteSpace = true; // otherwise the formatOutput option would have no effect
 	}
 
-	public function loadXML(string $source, int $options = 0) : bool
+	public function loadXML(string $source, int $options = 0): bool
 	{
 		return parent::loadXML($source, $options | LIBXML_BIGLINES);
 	}
